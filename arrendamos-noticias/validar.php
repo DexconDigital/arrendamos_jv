e456d817267425d07f14143272c820b17c116104
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-06-21 13:26:18 -0500

    Ajustes a pdo

--- arrendamos-noticias/validar.php
+++ arrendamos-noticias/validar.php
@@ -1,33 +1,36 @@
 <?php
 
-require("admin/conexion.php");
+require( "admin/conexion.php" );
 
-$con=conect();
+$con = conect();
 
+$nombre = $_REQUEST["user"];
+$password = $_REQUEST["pass"];
 
-$nombre=$_REQUEST["user"];
-$password=$_REQUEST["pass"];
+$qry = "SELECT usuario FROM usuarios WHERE usuario ='$nombre' and password ='$password' ";
 
-$qry="SELECT usuario FROM usuarios WHERE usuario ='$nombre' and password ='$password' ";
-$sql=mysqli_query($con,$qry);
+$result = $con->prepare( $qry );
+$result->execute();
 
-$row = mysqli_fetch_row($sql);
+$row = $result->fetch();
+$rowcount = $result->rowCount();
 
-if (mysqli_num_rows($sql) > 0) {
+if ( $rowcount > 0 ) {
     // output data of each row
-    session_start(); 
-      //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario 
-      $_SESSION["autentica"] = "SIP"; 
-      $_SESSION["usuarioactual"] =  $row[0]; 
-     header ("Location: admin/index.php");
-      //nombre del usuario logueado. 
-      //Direccionamos a nuestra página principal del sistema. 
+    session_start();
 
-     
+    //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario
+    $_SESSION["autentica"] = "SIP";
+
+    $_SESSION["usuarioactual"] =  $row[0];
+
+    header ( "Location: admin/index.php" );
+    //nombre del usuario logueado.
+    //Direccionamos a nuestra página principal del sistema.
+
 } else {
-        
- header ("Location: index.html");
 
+    header ( "Location: index.html" );
 
 }
 ?>
